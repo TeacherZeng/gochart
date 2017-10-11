1dc71c881be431fe6e2a96a31dc49e55bdcb5e61
fananchong <fananchong@126.com>
2017-10-11 18:32:20 +0800

    up

--- template_pie_html.go
+++ template_pie_html.go
@@ -8,7 +8,7 @@ var TemplatePieHtml = `{{define "T"}}
     <head>
 	    <meta http-equiv="refresh" content='{{.RefreshTime}}'>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Gochart - {{.ChartType}}</title>
+        <title>Gochart - {{.Title}}</title>
 
         <script type="text/javascript" src="/js/jquery-1.8.3.min.js"></script>
         <script type="text/javascript">
--- template_spline_html.go
+++ template_spline_html.go
@@ -9,7 +9,7 @@ var TemplateSplineHtml = `{{define "T"}}
     <head>
 	    <meta http-equiv="refresh" content='{{.RefreshTime}}'>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Gochart - {{.ChartType}}</title>
+        <title>Gochart - {{.Title}}</title>
         <link rel="stylesheet" href="/js/bootstrap.min.css">
         <script type="text/javascript" src="/js/jquery-1.8.3.min.js"></script>
         <script type="text/javascript">
